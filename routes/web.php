49475e315b0e95ffb5cc8d2cb4280e228df51468
kauaanrs <kauaanrs16@gmail.com>
2026-08-12 21:36:35 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\exerciciosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/exer1', [exerciciosController::class, 'abrirFormExer1']);
+
+Route::post('/exer1resp', [exerciciosController::class, 'respostaExer1']);
